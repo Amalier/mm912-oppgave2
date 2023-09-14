bc6b9a00184460312412a6a124de4d6b5e844864
Amalier <130658938+Amalier@users.noreply.github.com>
2023-09-14 22:05:06 +0200

    Add files via upload

---
+++ spill.js
@@ -0,0 +1,52 @@
+
+  const MIN_NUMBER = 1;
+  const MAX_NUMBER = 100;
+  let tries = 6;
+
+  const randomNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
+
+  const output = document.getElementById("outputtext");
+  const button = document.getElementById("guessButton");
+
+  button.addEventListener("click", updateButton);
+  playAgainButton.addEventListener("click", resetPage)
+  playAgainButton.disabled = true;
+
+
+  function resetPage() {
+    playAgainButton.disabled = false;
+    window.location.reload();
+
+  }
+
+  function updateButton() {
+
+    const guess = parseInt(document.getElementById("playerGuess").value);
+    const li = document.createElement("li");
+
+    if (isNaN(guess)) {
+      li.innerHTML = "Please enter a valid number.";
+    }
+
+    else if (guess === randomNumber) {
+      li.innerHTML = `You guessed ${guess}, Correct Guess!`;
+      button.disabled = true;
+      playAgainButton.disabled = false;
+
+
+    } else if (tries === 0) {
+      li.innerHTML = "you have no more tries <br/><h2><b>game over</h2></b> <br/>"
+      button.disabled = true;
+      playAgainButton.disabled = false;
+
+    } else if (guess < randomNumber) {
+      li.innerHTML = ` you guessed ${guess}, Too low, remianing tries ${tries}`;
+      tries --;
+    } else {
+      li.innerHTML = `you guessed ${guess}, Too high, remianing tries ${tries}`;
+      tries --;
+    }
+
+    output.appendChild(li)
+
+  }
